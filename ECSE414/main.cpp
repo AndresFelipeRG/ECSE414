1db7c1cbab2d84cc3fbd4498214d90298033b8d6
Andres Felipe Rincon Gamboa <andresfeliperincongamboa@Andress-MacBook-Pro.local>
2016-10-29 00:50:10 -0400

    Initial Commit

---
+++ ECSE414/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ECSE414
+//
+//  Created by Andres Felipe Rincon Gamboa on 2016-10-29.
+//  Copyright © 2016 Andres Felipe Rincon Gamboa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
